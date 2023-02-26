ae25a0e1a13c46b1acc6edef49e51540da95ce74
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-02-26 17:58:18 +0700

    Update and rename Day_con_tang_dan.cpp to DSA02024.cpp

--- DSA02024.cpp
+++ DSA02024.cpp
@@ -1,3 +1,22 @@
+/*
+Cho dãy số a[] có n phần tử là các số nguyên dương khác nhau từng đôi một. 
+Hãy liệt kê tất cả các dãy con có từ 2 phần tử trở lên của dãy a[] thỏa mãn tính chất tăng dần.
+
+Dãy con tạo được bằng cách lấy ra các phần tử trong dãy a[] nhưng vẫn giữ nguyên thứ tự ban đầu.
+
+Coi mỗi dãy con như một xâu ký tự với các phần tử cách nhau một khoảng trống, hãy liệt kê theo thứ tự từ điển.
+Input:
+4
+6 3 7 11
+Output:
+3 11
+3 7
+3 7 11
+6 11
+6 7
+6 7 11
+7 11
+*/
 #include <bits/stdc++.h>
 using namespace std;
 int n,a[20];
