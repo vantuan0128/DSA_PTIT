f6b7a3aa46b70ed081d272e975a5b9e061347414
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-02-26 18:16:07 +0700

    Update and rename Tong_nho_nhat.cpp to DSA03004.cpp

--- DSA03004.cpp
+++ DSA03004.cpp
@@ -1,3 +1,17 @@
+/*
+Cho mảng A[] gồm các số từ 0 đến 9. 
+Nhiệm vụ của bạn là tìm tổng nhỏ nhất của hai số được tạo bởi các số trong mảng A[]. 
+Chú ý, tất cả các số trong mảng A[] đều được sử dụng để tạo nên hai số.
+Input:
+2
+6
+6 8 4 5 2 3
+5
+5 3 0 7 4
+Output:
+604
+82
+*/
 #include <bits/stdc++.h>
 using namespace std;
 main(){
