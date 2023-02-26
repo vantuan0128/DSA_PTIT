cd87db4fabf5328826fabd4dcc3f2a216acb716b
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-02-26 17:05:53 +0700

    Update and rename Xau_nhi_phan_ke_tiep.cpp to DSA01001.cpp

--- DSA01001.cpp
+++ DSA01001.cpp
@@ -1,3 +1,6 @@
+/*
+Cho xâu nhị phân X[], nhiệm vụ của bạn là hãy đưa ra xâu nhị phân tiếp theo của X[]. Ví dụ X[] =”010101” thì xâu nhị phân tiếp theo của X[] là “010110”.
+*/
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
