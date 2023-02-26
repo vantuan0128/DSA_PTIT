bdcfdcdd1ee3c79db24b02eda59f34196d855acb
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-02-26 17:11:50 +0700

    Update and rename Hoan_vi_nguoc.cpp to DSA01006.cpp

--- DSA01006.cpp
+++ DSA01006.cpp
@@ -1,3 +1,7 @@
+/*
+Cho số nguyên dương N. Nhiệm vụ của bạn là hãy liệt kê tất cả các hoán vị của 1, 2, .., N theo thứ tự ngược. 
+Ví dụ với N = 3 ta có kết quả: 321, 312, 231, 213, 132, 123.
+*/
 #include <bits/stdc++.h>
 using namespace std;
 int n,mark[11]={0}, a[11];
@@ -11,10 +15,9 @@ void Try(int i){
 		if(!mark[j]){
 			mark[j]=1;
 			a[i]=j;
-			if(i==n) 
-			in();
+			if(i==n) in();
 			else{
-				Try(i+1);
+			  Try(i+1);
 			}
 			mark[j]=0;
 		}
