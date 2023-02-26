940ff9671acbe3263e4ea033c07ebc33b0d25211
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-02-26 18:05:50 +0700

    Update and rename So_xa_cach.cpp to DSA02033.cpp

--- DSA02033.cpp
+++ DSA02033.cpp
@@ -1,3 +1,17 @@
+/*
+Cho số nguyên dương N (2 < N <10). Một số nguyên dương K có N chữ số được gọi là số xa cách nếu thỏa mãn:
+
+K không chứa chữ số 0
+Tất cả các chữ số từ 1 đến N đều xuất hiện trong K đúng 1 lần
+Không có hai chữ số liên tiếp nào trong K có hiệu bằng 1. 
+Input: 
+2
+3
+4
+Output:
+2413
+3142
+*/
 #include <bits/stdc++.h>
 using namespace std;
 int n,a[30],b[30]={0};
