05e519c86fc9ddd092c096f29c8598654f1f623d
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-02-26 18:30:18 +0700

    Update and rename Lua_chon_toi_uu.cpp to DSA03024.cpp

--- DSA03024.cpp
+++ DSA03024.cpp
@@ -1,3 +1,18 @@
+/*
+Bạn được giao cho N công việc, công việc thứ i có thời gian bắt đầu là A[i] và kết thúc tại B[i]. Tại một thời điểm, bạn chỉ có thể làm một công việc.
+Bạn hãy lựa chọn các công việc một cách tối ưu sao cho số công việc làm được là nhiều nhất.
+Input:
+1
+6
+5 9
+1 2
+3 4
+0 6
+5 7
+8 9
+Output: 4
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 bool cmp(pair<int,int> a, pair<int,int> b){
