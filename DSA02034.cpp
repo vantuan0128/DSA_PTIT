56ddd48231c66a7b18481c01903f4b9e7446ab8c
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-02-26 18:08:20 +0700

    Update and rename Tro_choi_voi_cac_con_so.cpp to DSA02034.cpp

--- DSA02034.cpp
+++ DSA02034.cpp
@@ -1,3 +1,17 @@
+/*
+Nam có N thẻ bài, mỗi thẻ ghi một con số từ 1 tới N. Không có 2 thẻ nào ghi hai số trùng nhau. 
+Nam muốn xếp thứ tự các thẻ bài sao cho không có hai con số nào liền kề nhau ở cạnh nhau.
+
+Ví dụ với N = 4 thì cách xếp 3142 là hợp lệ nhưng cách xếp 1324 không hợp lệ.
+
+Hãy giúp Nam liệt kê tất cả các cách xếp thỏa mãn theo thứ tự từ điển.  
+Input: 
+1
+4
+Output:
+2413
+3142
+*/
 #include <bits/stdc++.h>
 using namespace std;
 int n,a[30],b[30]={0};
