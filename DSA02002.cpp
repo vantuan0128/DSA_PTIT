5ed9d3c6c0939747ec6755a8a5c55f546d9f2d99
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-02-26 17:38:14 +0700

    Update and rename Day_so_2.cpp to DSA02002.cpp

--- DSA02002.cpp
+++ DSA02002.cpp
@@ -1,3 +1,20 @@
+/*
+Cho dãy số A[] gồm n số nguyên dương. Tam giác đặc biệt của dãy số A[] là tam giác được tạo ra bởi n hàng, trong đó hàng thứ n là dãy số A[], 
+hàng i là tổng hai phần tử liên tiếp của hàng i+1 (1≤i≤n-1). Ví dụ A[] = {1, 2, 3, 4, 5}, khi đó tam giác được tạo nên như dưới đây:
+[48]
+[20, 28]
+[8, 12, 16]     
+[3, 5, 7, 9 ]
+[1, 2, 3, 4, 5 ]
+
+Input:
+1
+5
+1 2 3 4 5
+Output:
+[48] [20 28] [8 12 16] [3 5 7 9 ] [1 2 3 4 5 ]
+
+*/
 #include<bits/stdc++.h>
 using namespace std;
 int n,a[11];
