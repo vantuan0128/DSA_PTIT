f8f6127ceab315f96302f37db1e98e510866837c
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-02-27 17:10:39 +0700

    Update and rename Sap_xep_xen_ke.cpp to DSA06001.cpp

--- DSA06001.cpp
+++ DSA06001.cpp
@@ -1,3 +1,16 @@
+/*
+Cho mảng A[] gồm n số nguyên khác nhau. Hãy đưa ra các phần tử của mảng theo khuôn dạng lớn nhất, nhỏ nhất, lớn thứ hai, nhỏ thứ 2, …
+Ví dụ với A[] = {9, 7, 12, 8, 6, 5} ta đưa ra : 12, 5, 9, 6, 8, 7.
+Input:
+2
+7
+7 1 2 3 4 5 6
+8
+1 6 9 4 3 7 8 2
+Output:
+7 1 6 2 5 3 4
+9 1 8 2 7 3 6 4
+*/
 #include <bits/stdc++.h>
 using namespace std;
 void nhap(int a[], int n){
