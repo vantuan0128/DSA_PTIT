ac3f3847147e8a410c0b8ac8acaace1a1d998914
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-02-27 17:02:23 +0700

    Update and rename Giai_ma.cpp to DSA05029.cpp

--- DSA05029.cpp
+++ DSA05029.cpp
@@ -1,3 +1,14 @@
+/*
+Một bản tin M đã mã hóa bí mật thành các con số theo ánh xạ như sau: ‘A’->1, ‘B’->2, .., ‘Z’->26. Hãy cho biết có bao nhiêu cách khác nhau để giải mã bản tin M. 
+Ví dụ với bản mã M=”123” nó có thể được giải mã thành ABC (1 2 3), LC (12 3), AW(1 23).
+Input:
+2
+123
+2563
+Output:
+3
+2
+*/
 #include <bits/stdc++.h>
 using namespace std;
 int tinh(string s){
