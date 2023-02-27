56a55741b90b62b1b5076b09d17e5bdbaa852469
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-02-27 16:26:16 +0700

    Update and rename Phan_tu_khac_nhau.cpp to DSA04017.cpp

--- DSA04017.cpp
+++ DSA04017.cpp
@@ -1,3 +1,18 @@
+/*
+Cho hai mảng đã được sắp xếp A[] và B[] gồm N và N-1 phần tử. 
+Các phần tử của mảng A[] chỉ khác mảng B[] một phần tử duy nhất. Hãy tìm vị trí của phần tử khác nhau giữa A[] và B[].
+Input:
+2
+7
+2 4 6 8 9 10 12
+2 4 6 8 10 12
+6
+3 5 7 9 11 13
+3 5 7 11 13
+Output: 
+5
+4
+*/
 #include <bits/stdc++.h>
 using namespace std;
 using ll=long long;
