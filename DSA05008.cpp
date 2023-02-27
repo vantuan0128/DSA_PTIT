8eb83f8c14623578a876b15419509d8a0e23102d
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-02-27 16:48:54 +0700

    Update and rename Day_con_tong_bang_s.cpp to DSA05008.cpp

--- DSA05008.cpp
+++ DSA05008.cpp
@@ -1,3 +1,18 @@
+/*
+Cho N  số nguyên dương tạo thành dãy A={A1, A2, ..., AN}. 
+Tìm ra một dãy con của dãy A (không nhất thiết là các phần tử liên tiếp trong dãy) có tổng bằng S cho trước.
+Với mỗi bộ test, nếu bài toán vô nghiệm thì in ra “NO”, ngược lại in ra “YES”
+Input:
+2
+5 6
+1 2 4 3 5
+
+10 15
+2 2 2 2 2 2 2 2 2 2
+Output:
+YES
+NO
+*/
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
