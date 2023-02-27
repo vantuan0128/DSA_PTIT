a5c4b285b693c4d3d8740dd5d194d21dad4e50c9
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-02-27 16:11:15 +0700

    Update and rename Luy_thua.cpp to DSA04001.cpp

---
+++ DSA04001.cpp
@@ -0,0 +1,27 @@
+/*
+Cho số nguyên dương N và K. Hãy tính N mu K modulo 109+7.
+Input: 2
+	2 3
+	4 2
+Output: 8 
+	16
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int M=1e9+7;
+long long powM(int n, long k){
+	if(k == 0) return 1;
+	if(k == 1) return n % M;
+	long long x = powM(n,k/2);
+	if(k%2==0) return (x * x) % M;
+	return (n * (x * x % M)) % M;
+}
+main(){
+	int t; cin>>t;
+	while(t--){
+		int n;
+		long k;
+		cin>>n>>k;
+		cout<<powM(n,k)<<endl;
+	}
+}
