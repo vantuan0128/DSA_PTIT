a711ccc68a3e5d314bd4db51af74cc0d07407237
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-02-27 17:00:56 +0700

    Update and rename Cai_tui.cpp to DSA05027.cpp

--- DSA05027.cpp
+++ DSA05027.cpp
@@ -1,3 +1,14 @@
+/*
+Một người có cái túi thể tích V (V<1000). Anh ta có N đồ vật cần mang theo (N≤1000), mỗi đồ vật có thể tích là A[i] (A[i]≤100) và giá trị là C[i] (C[i]≤100). 
+Hãy xác định tổng giá trị lớn nhất của các đồ vật mà người đó có thể mang theo, sao cho tổng thể tích không vượt quá V.
+Input:
+1
+15 10
+5 2 1 3 5 2 5 8 9 6 3 1 4 7 8
+1 2 3 5 1 2 5 8 7 4 1 2 3 2 1
+Output: 15
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 //Trong luong tui phu thuoc 2 yeu to: So do vat dang xet
