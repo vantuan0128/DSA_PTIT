22623625ef46e78a0a2ec6a7ace546c76233f5f6
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-03-04 16:43:27 +0700

    Update and rename Phan_tu_lon_nhat_trong_day_con_c2.cpp to DSA06031_c2.cpp

--- DSA06031_c2.cpp
+++ DSA06031_c2.cpp
@@ -1,3 +1,16 @@
+/*
+Cho dãy số A[] gồm có N phần tử và số nguyên K.
+Với mỗi dãy con liên tiếp có độ dài bằng K (từ trái sang phải), bạn hãy in ra phần tử lớn nhất trong dãy con này.
+Input:
+2
+9 3
+1 2 3 1 4 5 2 3 6
+10 4
+8 5 10 7 9 4 15 12 90 13
+Output:
+3 3 4 5 5 5 6
+10 10 10 15 15 90 90
+*/
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
