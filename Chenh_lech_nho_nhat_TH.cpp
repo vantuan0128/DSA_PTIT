05982beab18254c9068e9fb69e5b8fe03fe16ab9
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-03-04 17:10:45 +0700

    Update Chenh_lech_nho_nhat_TH.cpp

--- Chenh_lech_nho_nhat_TH.cpp
+++ Chenh_lech_nho_nhat_TH.cpp
@@ -9,13 +9,13 @@ void SinhKt(){
 	while(i>=1&&a[i]>a[i+1]) --i;
 	if(i==0) ok=true;
 	else{
-		int j=k;
-		while(a[i]>a[j]) --j;
+		int j = k;
+		while(a[i] > a[j]) --j;
 		swap(a[i],a[j]);
-		for(int h=0; h<n;h++)
+		for(int h = 0; h < n; h++)
 			swap(v[h][i-1],v[h][j-1]);
 		reverse(a+i+1,a+k+1);
-		for(int h=0;h<n;h++){
+		for(int h = 0;h < n; h++){
 			reverse(v[h].begin()+i,v[h].end());
 		}
 	}
@@ -30,7 +30,7 @@ int main(){
 	while(!ok){
 		vector<string> s=v;
 		sort(s.begin(),s.end());
-		res=min(res,stoll(s[n-1])-stoll(s[0]));
+		res = min(res,stoll(s[n-1])-stoll(s[0]));
 		SinhKt();
 	}
 	cout<<res<<endl;
