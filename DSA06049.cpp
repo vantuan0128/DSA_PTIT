a72130c56a758938d590b9421a877e2492ed2aa3
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-03-04 17:16:11 +0700

    Update and rename Dem_cap_DSA06049.cpp to DSA06049.cpp

--- DSA06049.cpp
+++ DSA06049.cpp
@@ -1,3 +1,16 @@
+/*
+Cho mảng A[] gồm n số nguyên dương và số k. Nhiệm vụ của bạn là đếm số các cặp phần tử có hiệu nhỏ hơn k. 
+Ví dụ A[] = {1, 10, 4, 2 }, k=3 ta nhận được kết quả là 2 tương ứng với hiệu các cặp (1, 2), (4, 2).
+Input:
+2
+4 3
+1 10 4 2
+3 5
+2 3 4
+Output:
+2
+3
+*/
 #include <bits/stdc++.h>
  
 #define faster()                  \
