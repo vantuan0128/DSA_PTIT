dcc179d16ec2a24df7f1bbd4016999ccbd872f0a
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-03-04 16:56:56 +0700

    Update and rename Day_tam_giac_dai_nhat.cpp to DSA06035.cpp

--- DSA06035.cpp
+++ DSA06035.cpp
@@ -1,3 +1,19 @@
+/*
+Cho dãy số A[] gồm có N phần tử.
+Một dãy con liên tiếp được gọi là dãy tam giác nếu như dãy đó tăng dần rồi lại giảm dần, hay tồn tại i, j, k sao cho A[i] ≤ A[i+1] ≤ … ≤ A[k] >= A[k+1] >= … >= A[j].
+Nhiệm vụ của bạn là hãy tìm dãy con liên tiếp là dãy tam giác có độ dài lớn nhất.
+Lưu ý: Dãy đơn điệu không giảm hoặc không tăng cũng là dãy tam giác.
+Ví dụ A[] = {10, 20, 30, 40} là một dãy tam giác.
+Input:
+2
+6
+12 4 78 90 45 23
+8
+20 4 1 2 3 4 2 10
+Output:
+5
+5
+*/
 #include<bits/stdc++.h>
 
 using namespace std;
