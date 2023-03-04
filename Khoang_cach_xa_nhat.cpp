c2844490a4d9a94f8873176a0249ec1cfa216ede
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-03-04 16:49:38 +0700

    Add files via upload

---
+++ Khoang_cach_xa_nhat.cpp
@@ -0,0 +1,74 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+long long bs(long long a[], long long l, long long r, long long x) {
+    long long res = -1;
+    while(l <= r) {
+        int m = (l+r)/2;
+        if(a[m] < x) {
+            res = m;
+            l = m +1;
+        }
+        else {
+            r = m-1;
+        }
+    }
+    return res;
+}
+
+int solve(int a[], int n) {
+    int res = -1;
+    for(int i = 0; i < n; i ++) {
+        for(int j = n-1; j > i; j--) {
+            if(a[j] > a[i]) {
+                res = max(res, j - i);
+            }
+        }
+    }
+    return res;
+}
+
+int findMaxDistance(int A[], int N) {
+    int i = 0, j = 0;
+    for (int k = 1; k < N; k++) {
+        if (A[k] > A[j]) {
+            j = k;
+            if (A[j] > A[i]) {
+                i = j;
+            }
+        }
+    }
+    return j - i;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        vector<pair<int,int>> v;
+        for(int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            v.push_back({x, i});
+        }
+        sort(v.begin(), v.end());
+        int res = -1, MIN = v[0].second, k = v[0].first;
+        for(int i = 1; i < n; i++) {
+            if(v[i].first > k) {
+                res = max(res, v[i].second - MIN);
+            }
+            if(MIN > v[i].second) {
+                k = v[i].first;
+                MIN = v[i].second;
+            }
+
+        }
+        cout << res;
+        cout << endl;
+    }
+    return 0;
+}
