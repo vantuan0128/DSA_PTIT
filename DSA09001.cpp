c56377414f3e370d90932a4d69f1b6b37d010a13
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-03-18 17:37:26 +0700

    Update and rename Chuyen_dscanh_sang_dske.cpp to DSA09001.cpp

--- DSA09001.cpp
+++ DSA09001.cpp
@@ -1,3 +1,25 @@
+/*
+Cho đồ thị vô hướng G=<V, E> được biểu diễn dưới dạng danh sách cạnh. Hãy viết chương trình thực hiện chuyển đổi biểu diễn đồ thị dưới dạng danh sách kề.
+Input:
+1
+6  9
+1  2
+1  3 
+2  3
+2  5
+3  4
+3  5
+4  5
+4  6
+5  6
+Output:
+1: 2 3
+2: 1 3 5
+3: 1 2 4 5
+4: 3 5 6
+5: 2 3 4 6
+6: 4 5
+*/
 #include <bits/stdc++.h>
 using namespace std;
 using ll=long long;
