cabb7af3ea62a0ea11ab633b7a8d8793d03fa15f
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-03-23 22:37:46 +0700

    Update and rename DSA06031_c1.cpp to DSA06031.cpp

--- DSA06031.cpp
+++ DSA06031.cpp
@@ -32,3 +32,31 @@ int main() {
     }
     return 0;
 }
+
+//Cach 2
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int t; cin>>t;
+	while(t--){
+		int n,k; cin>>n>>k;
+		multiset<int> ms;
+		int a[n];
+		for(int i=0;i<n;i++){
+			cin>>a[i];
+		}
+		for(int i=0;i<k;i++){
+			ms.insert(a[i]);
+		}
+		for(int i=k;i<n;i++){
+			cout<<*ms.rbegin()<<" ";
+			ms.erase(ms.find(a[i-k]));
+			ms.insert(a[i]);
+		}
+		cout<<*ms.rbegin()<<endl;
+	}
+	return 0;
+}
