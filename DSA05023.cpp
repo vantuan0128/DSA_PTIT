18436cfbf1aecab4911e2559e25681f3362e047d
TunazZz <121681379+vantuan0128@users.noreply.github.com>
2023-05-11 00:39:43 +0700

    Update and rename Tong_cac_xau_con.cpp to DSA05023.cpp

--- DSA05023.cpp
+++ DSA05023.cpp
@@ -1,3 +1,14 @@
+/*
+Cho số nguyên dương N được biểu diễn như một xâu ký tự số. 
+Nhiệm vụ của bạn là tìm tổng của tất cả các số tạo bởi các xâu con của N. Ví dụ N=”1234” ta có kết quả là 1670 = 1 + 2 + 3 + 4 + 12 + 23 + 34 + 123 + 234 + 1234.
+Input:
+2
+1234
+421
+Output:
+1670
+491
+*/
 #include <bits/stdc++.h>
 using namespace std;
 int chuyen(char c){
