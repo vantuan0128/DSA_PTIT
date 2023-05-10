24ecb6dfcf95694d54d8fa9970ed55bc04f9012f
TunazZz <121681379+vantuan0128@users.noreply.github.com>
2023-05-11 00:10:44 +0700

    Update and rename Gia_tri_nho_nhat_cua_bieu_thuc.cpp to DSA03007.cpp

--- DSA03007.cpp
+++ DSA03007.cpp
@@ -1,3 +1,18 @@
+/*
+Cho mảng A[], B[] đều có N phần tử. Nhiệm vụ của bạn là tìm giá trị nhỏ nhất của  biểu thức 
+P = A[0]*B[0] + A[1]*B[1] + ..+A[N-1]*B[N-1] bằng cách tráo đổi vị trí các phần tử của cả mảng A[] và B[].
+Input:
+2
+7
+1 6 3 4 5 2 7
+1 1 1 2 3 4 3
+7
+1 6 3 5 5 2 2
+0 1 9 0 1 2 3
+Output:
+45
+27
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
