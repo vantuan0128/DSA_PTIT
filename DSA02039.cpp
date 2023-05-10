d2fe8ad47d975040eb33f8760cfd8987542f1392
TunazZz <121681379+vantuan0128@users.noreply.github.com>
2023-05-11 00:06:23 +0700

    Update and rename Phan_tich_so_2.cpp to DSA02039.cpp

--- DSA02039.cpp
+++ DSA02039.cpp
@@ -1,3 +1,17 @@
+/*
+Cho số nguyên dương N. Nhiệm vụ của bạn là hãy liệt kê tất cả các cách phân tích số tự nhiên N thành tổng các số tự nhiên nhỏ hơn hoặc bằng N. 
+Phép hoán vị của một cách được xem là giống nhau. 
+Ví dụ với N = 5 ta có kết quả là: (5), (4, 1), (3, 2), (3, 1, 1), (2, 2, 1), (2, 1, 1, 1), (1, 1, 1, 1, 1) .
+Input:
+2
+4
+5
+Output:
+5
+(4) (3 1) (2 2) (2 1 1) (1 1 1 1)
+7
+(5) (4 1) (3 2) (3 1 1) (2 2 1) (2 1 1 1) (1 1 1 1 1)
+*/
 #include <bits/stdc++.h>
 using namespace std;
 int n,X[100];
