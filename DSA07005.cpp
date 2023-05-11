3ee08f7ff70992bae34253f879d67e1ad27116d0
TunazZz <121681379+vantuan0128@users.noreply.github.com>
2023-05-11 17:19:11 +0700

    Update and rename Bieu_thuc_tuong_duong.cpp to DSA07005.cpp

--- DSA07005.cpp
+++ DSA07005.cpp
@@ -1,3 +1,15 @@
+/*
+Cho biểu thức đúng P chỉ bao gồm các phép toán +, -, các toán hạng cùng với các ký tự ‘(’, ‘)’. Hãy bỏ tất cả các ký tự ‘(’, ‘)’ trong P để nhận 
+được biểu thức tương đương.
+Ví dụ với P = a – (b + c) ta có kết quả P = a – b – c .
+Input:
+2
+a–(b+c)
+a-(b-c-(d+e))-f
+Output:
+a-b-c
+a-b+c+d+e-f
+*/
 #include <bits/stdc++.h>
 
 using namespace std;
