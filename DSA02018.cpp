a3f42abe5c85dd150a0627cbef02ee83267e75be
TunazZz <121681379+vantuan0128@users.noreply.github.com>
2023-05-11 18:02:04 +0700

    Update and rename Bieu_thuc_toan_hoc.cpp to DSA02018.cpp

--- DSA02018.cpp
+++ DSA02018.cpp
@@ -1,3 +1,22 @@
+/*
+Cho 5 số nguyên dương A, B, C, D, E. Bạn có thể hoán vị các phần tử cho nhau, hãy đặt các dấu biểu thức +, -, * sao cho biểu thức sau đúng:
+
+[[[A o(1) B] o(2) C] o(3) D] o(4) E = 23
+
+Trong đó: o(1) … o(4) là các phép toán +, -, *.
+Dòng đầu tiên là số lượng bộ test T (T ≤ 20).
+
+Mỗi test gồm 5 số nguyên dương A, B, C, D, E có giá trị không vượt quá 100.
+Input:
+3
+1 1 1 1 1
+1 2 3 4 5
+2 3 5 7 11
+Output:
+NO
+YES
+YES
+*/
 #include <bits/stdc++.h>
 using namespace std;
 int number[6], OK, chuaxet[6];
