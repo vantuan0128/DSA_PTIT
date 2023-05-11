0e7860e5cc301863056b23ecceac5b5e5bd3d3c5
TunazZz <121681379+vantuan0128@users.noreply.github.com>
2023-05-11 16:32:29 +0700

    Update and rename Sap_xep_chan_le.cpp to DSA06044.cpp

--- DSA06044.cpp
+++ DSA06044.cpp
@@ -1,3 +1,11 @@
+/*
+Cho dãy số a[] có n phần tử, đánh số từ 1 đến n. Hãy sắp xếp các phần tử ở vị trí lẻ theo thứ tự tăng dần, các phần tử ở vị trí chẵn theo thứ tự giảm dần.
+Input:
+4
+1 2 3 4
+Output:
+1 4 3 2
+*/
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
