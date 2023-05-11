10a61e1d6b07c4f369935ddd0051f9953d1db67f
TunazZz <121681379+vantuan0128@users.noreply.github.com>
2023-05-11 17:12:19 +0700

    Update and rename Tap_con_lien_ke_phia_truoc.cpp to DSA01018.cpp

--- DSA01018.cpp
+++ DSA01018.cpp
@@ -1,3 +1,16 @@
+/*
+Cho hai số N, K và một tập con K phần tử X[] =(X1, X2,.., XK) của 1, 2, .., N. Nhiệm vụ của bạn là hãy đưa ra tập con K phần tử trước đó của X[].
+Ví dụ N=5, K=3, X[] ={2, 3, 5} thì tập con trước đó của X[] là {2, 3, 4}. Chú ý nếu tập con trong input là đầu tiên thì trước đó là tập con cuối cùng.
+Input:
+2
+5  3
+2  3  5
+5  3
+1  2  3
+Output:
+2 3 4
+3 4 5
+*/
 #include <bits/stdc++.h>
 using namespace std;
 int n, k, C[1001];
