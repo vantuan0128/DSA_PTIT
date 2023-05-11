789927beb8a8db3cf7318430ab81ff85ec394c30
TunazZz <121681379+vantuan0128@users.noreply.github.com>
2023-05-11 16:45:51 +0700

    Update and rename Bien_doi_S_T.cpp to DSA08009.cpp

--- DSA08009.cpp
+++ DSA08009.cpp
@@ -1,3 +1,30 @@
+/*
+Cho hai số nguyên dương S và T (S, T<10000) và hai thao tác (a), (b) dưới đây:
+
+Thao tác (a): Trừ S đi 1  (S = S-1) ;
+
+Thao tác (b): Nhân S với 2 ( S = S*2);
+
+Hãy dịch chuyển S thành T sao cho số lần thực hiện các thao tác (a), (b) là ít nhất.
+Ví dụ với    S =2, T=5 thì số các bước ít nhất để dịch chuyển S thành T thông qua 4 thao tác sau:
+
+Thao tác (a): 2*2 = 4;
+
+Thao tác (b): 4-1 = 3;
+
+Thao tác (a): 3*2 = 6;
+
+Thao tác (b): 6-1 = 5;
+Input:
+3
+2 5
+3 7
+7 4
+Output:
+4
+4
+3
+*/
 #include <bits/stdc++.h>
 using namespace std;
 using ll=long long;
