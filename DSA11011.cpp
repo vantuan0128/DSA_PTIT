7958bfff4c60e4d2b5b0ca0b662f1eaced2c767f
TunazZz <121681379+vantuan0128@users.noreply.github.com>
2023-05-11 17:02:25 +0700

    Update and rename Cay_bieu_thuc_1.cpp to DSA11011.cpp

--- DSA11011.cpp
+++ DSA11011.cpp
@@ -1,3 +1,17 @@
+/*
+Cây biểu thức là một cây nhị phân trong đó mỗi node trung gian là một phép toán, mỗi node lá là một toán hạng. 
+Ví dụ với biểu thức P = 3 + ((5+9)*2) sẽ được biểu diễn như cây dưới đây.
+Đối với cây biểu thức, duyệt theo thứ tự trước ta sẽ được biểu thức tiền tố, duyệt theo thứ tự sau ta sẽ được biểu thức hậu tố, 
+duyệt theo thứ tự giữa ta được biểu thức trung tố. Chú ý, cây biểu thức luôn là cây nhị phân đầy (mỗi node trung gian đều có hai node con).
+Cho biểu thức hậu tố P, hãy sử dụng cây biểu thức để đưa ra biểu thức trung tố tương ứng với biểu thức P.
+Input:
+2
+ab+ef*g*-
+wlrb+-*
+Output:
+a + b - e * f * g
+w * l - r + b
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
